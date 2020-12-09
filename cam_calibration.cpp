6768dd9cb88fb6c93c7b479ad16a59e20cb5fa50
Michael Doody <michaeldoody2017@u.northwestern.edu>
2020-12-08 19:34:57 -0600

    calibration function

--- cam_calibration.cpp
+++ cam_calibration.cpp
@@ -7,6 +7,34 @@
 using namespace cv;
 using namespace std;
 
+Mat src, src_gray;
+Mat dst, detected_edges;
+Mat cdst, cdstP;
+
+int lowThreshold = 8;
+const int max_lowThreshold = 100;
+const int kernel_size = 3;
+
+static void Calibrate(int, void*)
+{
+    blur( src_gray, detected_edges, Size(3,3) );
+    Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*3, kernel_size );
+    dst = Scalar::all(0);
+    src.copyTo( dst, detected_edges);
+    
+    // Copy edges to the images that will display the results in BGR
+    cvtColor(detected_edges, cdst, COLOR_GRAY2BGR);
+    cdstP = cdst.clone();
+    
+    // Standard Hough Line Transform
+    vector<Vec2f> lines; // will hold the results of the detection
+    HoughLines(detected_edges, lines, 1, CV_PI/180, 150, 0, 0 ); // runs the actual detection
+    
+
+    
+    imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst);
+}
+
 int main( int argc, char** argv )
 {
   //open the video file for reading
@@ -35,7 +63,7 @@ int main( int argc, char** argv )
     cvtColor( src, src_gray, COLOR_BGR2GRAY );
     namedWindow( "RasPi Cam", WINDOW_AUTOSIZE );
     imshow("RasPi Cam", src);
-    CannyThreshold(0, 0);
+    Calibrate(0, 0);
 
     if (waitKey(10) == 27)
     {
